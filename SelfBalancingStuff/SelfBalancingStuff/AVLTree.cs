cb513e4e6a9f68a16f4c5dd84afff34ffd844e9b
Elysia Lopez <elysialopez528@gmail.com>
2021-03-25 22:11:24 -0700

    Started working on AVL tree; made Node class and started Insert function for the AVLTree class.

---
+++ SelfBalancingStuff/SelfBalancingStuff/AVLTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfBalancingStuff
+{
+    class AVLTree<T>
+        where T : IComparable<T>
+    {
+        Node<T> Root { get; set; }
+        public AVLTree()
+        {
+            Root = null;
+        }
+
+        public void Insert(T val)
+        {
+            var node = new Node<T>(val);
+            if(Root == null)
+            {
+                Root = node;
+                return;
+            }
+            var temp = Root;
+            while(true)
+            {
+                if(val.CompareTo(temp.Value) < 0)
+                {
+                    if(temp.Left == null)
+                    {
+                        temp.Left = node;
+                        return;
+                    }
+                    temp = temp.Left;
+                }
+                else
+                {
+                    if(temp.Right == null)
+                    {
+                        temp.Right = node;
+                        return;
+                    }
+                    temp = temp.Right;
+                }
+            }
+
+        }
+    }
+}
---
+++ SelfBalancingStuff/SelfBalancingStuff/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfBalancingStuff
+{
+    class Node<T>
+        where T : IComparable<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public int Balance
+        {
+            get
+            {
+                int leftHeight = 0;
+                int rightHeight = 0;
+                if (Left != null) leftHeight = Left.Height;
+                if (Right != null) rightHeight = Right.Height;
+                return rightHeight - leftHeight;
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                if (Left == null && Right == null) return 1;
+                int maxHeight = Left.Height.CompareTo(Right.Height) >= 0 ? Left.Height : Right.Height;
+                return maxHeight;
+            }
+        }
+        public Node(T val)
+        {
+            Value = val;
+        }
+    }
+}
